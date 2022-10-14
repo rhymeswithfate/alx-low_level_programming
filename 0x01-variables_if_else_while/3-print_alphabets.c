9e6e99cc20d1cc1d75cd3631bd94087cd7aea2a8
chayt <kasotechayt@outlook.com>
2022-10-14 14:03:00 -0700

    Fixes, no. 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,10 @@
 int main(void)
 {
 char ch;
-int i = 1;
-while (i < 53)
-{
-if(i < 27)
-{
-for(ch = 'a'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-}
-else
-{
-for(ch = 'A'; ch <= 'Z'; ch++)
+for (ch = 'A'; ch <= 'Z'; ch++)
 putchar(ch);
-}
-i++;
-}
 putchar('\n');
 return (0);
 }
